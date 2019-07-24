1b1951f59d920d18747cd90beb7241cd5bb7bab0
DanielaCirstoiu <35976546+DanielaCirstoiu@users.noreply.github.com>
2019-07-24 18:12:32 +0300

    creat modele si migratii sesiune 2

--- tema1/routes/web.php
+++ tema1/routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', 'AboutController@index');
+
+Route::get('/employees', 'EmployeesController@show');
+Route::post('/employees', 'EmployeesController@create');
